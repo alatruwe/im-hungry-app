e0ac6e7a978431f8c85677c01ed5cd29c21a2c87
adelinel <adeline.latruwe@gmail.com>
2020-11-23 11:26:37 -0800

    created js file + function stubs

---
+++ app.js
@@ -0,0 +1,45 @@
+'use strict';
+/* what I need to call the API:
+- API key: 306dc4a6c06d4f93835642153baafd56
+- Base URL: https://api.spoonacular.com/
+*/
+
+/******** RENDER FUNCTIONS ********/
+//function homeScreen()
+//function displayRecipe(details)
+//function displayInstructions(instructions)
+//function endRecipe()
+
+
+/******** API CALL FUNCTIONS ********/
+//function getRandomRecipe()
+//function getRecipeInstructions()
+
+
+/******** EVENT HANDLER FUNCTIONS ********/
+//function handleStartClick()
+  //event click button
+  //api call for recipe details - function getRandomRecipe() => return details
+  //function displayRecipe(details)
+
+//function handleAcceptRecipe()
+  //event click OK button
+  //api call for recipe instructions - function getRecipeInstructions() => return instructions
+  //function displayInstructions(instructions)
+
+//function handleNewRecipe()
+  //event click NO button 
+  //api call for recipe details - function getRandomRecipe() => return details
+  //function displayRecipe(details)
+
+//function handleEndRecipe()
+  //event click button Done
+  //function homeScreen()  
+
+//function startApp()
+  //handleStartClick()
+  //handleAcceptRecipe()
+  //handleNewRecipe()
+  //handleEndRecipe()
+
+//$(startApp);
